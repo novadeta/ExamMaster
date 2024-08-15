e2a73f769f799a2692474cc1563a285dfb41b36c
Ade Oktaviano Arrahman <adeoktaviano50@gmail.com>
2024-08-16 06:55:57 +0700

    section 1,2,3,4

--- ExamMaster/tailwind.config.js
+++ ExamMaster/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["*{html,js}"],
   theme: {
     extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1180px',
+        '2xl': '1280px',
+      },
       colors:{
         'purple': '#E43EFF'
       }
