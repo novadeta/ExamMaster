dd9a9b9414e2a2384c8011f740df5f3ec97bc478
EevnxX1 <miftamartin1@gmail.com>
2024-08-16 21:55:20 +0700

    tahap jadi

--- ExamMaster/tailwind.config.js
+++ ExamMaster/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       },
       colors:{
         'purple': '#E43EFF'
+      },
+      boxShadow: {
+        'card': '10px 0 2px -5px #E43EFF, 0 -4px 10px -5px #000000'
       }
     },
   },
